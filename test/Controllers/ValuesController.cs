588ce5f6b3b60b4468b62871ffca5ad789a52edb
forwork111 <fuyj11@centaline.com.cn>
2020-11-06 14:04:57 +0800

    添加项目文件。

---
+++ test/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+namespace test.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+         
+
+
+        [HttpGet("test3")]
+        public string test3()
+        {
+            return "11111";
+        }
+    }
+
+}
